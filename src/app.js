b526804050311d5f3944379418aacec138129f84
aravindashokk <aravindkayal@gmail.com>
2025-04-13 19:42:46 -0500

    feat(backend): handle API with GET,POST and DELETE method

--- src/app.js
+++ src/app.js
@@ -2,7 +2,20 @@ const express = require("express");
 
 const app = express();
 
-app.use("/",(req, res) => {
+app.get("/user",(req,res)=>{
+    res.send({firstName: "Aravind", lastName: "Ashok"});
+});
+
+app.post("/user",(req,res)=>{
+    res.send("Data saved successfully to the database");
+});
+
+app.delete("/user",(req,res)=> {
+    res.send("Deleted successfully");
+})
+
+//match all the HTTP method API calls to /test
+app.use("/test",(req, res) => {
     res.send("Hello from the server");
 });
 
