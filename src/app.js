5341a65ab36e8dc476e596dec4c666edeb2be4a8
aravindashokk <aravindkayal@gmail.com>
2025-04-16 10:29:01 -0500

    feat(backend) :  create userSchema and a new instance of User model to add user into the database POST /signup

--- src/app.js
+++ src/app.js
@@ -1,7 +1,25 @@
 const express = require("express");
 const connectDB = require("./config/database");
+const User = require("./models/user");
 
 const app = express();
+
+app.post("/signup", async (req, res) => {
+    const userObj = {
+        firstName: "Dhoni",
+        lastName: "MS",
+        emailId: "dhoni@gmail.com",
+        password: "dhoni@123",
+    }
+
+    //Creating a new instance of the User Model
+    const user = new User(userObj)
+
+    await user.save();
+
+    res.send("User Added successfully");
+
+});
 
 
 connectDB().then(() => {
