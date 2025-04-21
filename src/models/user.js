7ae308572db24034acdf123c545ccfe0660da2d8
aravindashokk <aravindkayal@gmail.com>
2025-04-21 12:52:28 -0500

    feat(backend): added API level validation

--- src/models/user.js
+++ src/models/user.js
@@ -5,17 +5,19 @@ const userSchema = new mongoose.Schema({
     lastName: { type: String },
     emailId: { type: String, lowercase: true, trim: true, required: true, unique: true },
     password: { type: String, required: true, },
-    age: { type: Number, min: 18  },
-    gender: { type: String, validate(value) {
-        if (!["male","female","others"].includes(value)) {
-            throw new Error("Gender data not valid");
+    age: { type: Number, min: 18 },
+    gender: {
+        type: String, validate(value) {
+            if (!["male", "female", "others"].includes(value)) {
+                throw new Error("Gender data not valid");
+            }
         }
-    } },
+    },
     photoUrl: { type: String, default: "https://geographyandyou.com/images/user-profile.png" },
     about: { type: String, default: "This is a default description of the user" },
     skills: { type: [String], }
-},{
-    timestamps : true,
+}, {
+    timestamps: true,
 });
 
 
