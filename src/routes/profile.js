840fa4ce3e89a6c17da8c4c1a82f9f3fdc85eaf9
aravindashokk <aravindkayal@gmail.com>
2025-05-01 14:25:16 -0500

    feat(backend): POST /profiilr/view and validateEditProfileData

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { userAuth } = require("../middlewares/auth");
+const { validateEditProfileData } = require("../utils/validation");
 
 const profileRouter = express.Router();
 
 //Get profile data
-profileRouter.get("/profile", userAuth, async (req, res) => {
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
     try {
         const user = req.user
         if (!user) {
@@ -14,6 +15,24 @@ profileRouter.get("/profile", userAuth, async (req, res) => {
         res.send(user);
 
     } catch (err) {
+        res.status(400).send("Something went wrong: " + err.message);
+    }
+});
+ 
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+    try{
+        if(!validateEditProfileData(req)){
+            throw new Error("Invalid edit request!");
+        }
+        const loggedInUser = req.user;
+        Object.keys(req.body).forEach((key)=>(loggedInUser[key] = req.body[key]));
+        await loggedInUser.save();      
+
+        res.json({
+            message: `${loggedInUser.firstName}, your profile updated successfully!`,
+            data: loggedInUser,
+        });
+    } catch(err) {
         res.status(400).send("Something went wrong: " + err.message);
     }
 });
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -13,6 +13,15 @@ const  validateSignUpData = (req) => {
         throw new Error(" Please enter a strong password!");
     }
 };
+
+const validateEditProfileData = (req) => {
+    const allowedEditFields = ["firstName", "lastName", "age" ,"gender", "photoUrl" ,"about" ,"skills"];
+    const isEditAllowed = Object.keys(req.body).every((field)=>allowedEditFields.includes(field));
+    return isEditAllowed;
+};
+
+
 module.exports={
     validateSignUpData,
+    validateEditProfileData
 };
