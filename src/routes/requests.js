8d9c80a066510ad292c24a6484780e48cf265d57
aravindashokk <aravindkayal@gmail.com>
2025-05-07 20:08:55 -0500

    feat(backend): Post /request/send/:status/:toUserId and index in database

--- src/routes/requests.js
+++ src/routes/requests.js
@@ -1,15 +1,57 @@
 const express = require('express');
 const { userAuth } = require("../middlewares/auth");
+const connectionRequest = require('../models/connectionRequest');
+const User = require('../models/user');
 
 const requestRouter = express.Router();
 
-requestRouter.post("/sendConnectionRequest", userAuth, async (req, res) => {
+requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res) => {
     try {
-        const user = req.user
-        console.log("Send a connection request");
+        const fromUserId = req.user._id;
+        const toUserId = req.params.toUserId;
+        const status = req.params.status;
 
-        res.send(user.firstName + " sent the connecton request");
+        const allowedStatuses = ["ignored", "interested"];
+        if (!allowedStatuses.includes(status)) {
+            return res.status(400).json({
+                message: "Invalid status type: " + status,
+            });
+        } 
 
+        const toUser = await User.findById(toUserId);
+        if (!toUser) {
+            return res.status(404).json({
+                message: "User not found",
+            });
+        }
+
+
+        const existingConnectionRequest = await connectionRequest.findOne({
+            $or: [
+                { fromUserId, toUserId },
+                { fromUserId: toUserId, toUserId: fromUserId },
+            ],
+        });
+
+        
+
+        if(existingConnectionRequest) {
+            return res.status(400).json({
+                message: "Connection request already exists",
+            });
+        }
+
+        const connectionRequests = new connectionRequest({
+            fromUserId,
+            toUserId,
+            status,
+        });
+
+        const data = await connectionRequests.save();
+        res.json({
+            message: req.user.firstName+" "+status+" "+toUser.firstName,
+            data: data,
+        });
     } catch (err) {
         res.status(400).send("Something went wrong: " + err.message);
     }
